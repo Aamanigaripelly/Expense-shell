04b9d60a6e78971254b9cb5583deb01d3615acfd
Aamani <aamani.garipelly@gmail.com>
2023-11-04 20:37:11 +0530

    expense

--- Frontend.sh
+++ Frontend.sh
@@ -1,12 +1,28 @@
-dnf install nginx -y
-cp expense.config /etc/nginx/default.d/expense.conf
+log_file=/tmp/expense.log
+color="\e[36m"
 
-rm -rf /usr/share/nginx/html/*
+echo -e "${color} Installing Nginx \e[0m"
+dnf install nginx -y &>>$log_file
+echo$?
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "${color} Copy Expense Config file \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo$?
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+echo -e "${color} Clean Old Nginx Content \e[0m"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+echo$?
 
-systemctl enable nginx
-systemctl restart nginx
+echo -e "${color} Download Frontend Application Code \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+echo$?
+
+echo -e "${color} Extract Downloaded Application Content \e[0m"
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
+echo$?
+
+echo -e "${color} Starting Nginx Service \e[0m"
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
+echo$?
--- backend.sh
+++ backend.sh
@@ -1,7 +1,23 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-dnf install nodejs -y
-cp backend.service /etc/systemd/system/backend.service
+log_file=/tmp/expense.log
+color="\e[36m"
+
+echo -e "${color} Disable NodeJS default Version \e[0m"
+dnf module disable nodejs -y &>>$log_file
+echo$?
+
+
+echo -e "${color} Enable NodeJS 18 Version \e[0m"
+dnf module enable nodejs:18 -y &>>$log_file
+echo$?
+
+
+echo -e "${color} Install NodeJS \e[0m"
+dnf install nodejs -y &>>$log_file
+echo$?
+
+echo -e "${color} Copy Backend Service File \e[0m"
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
+echo$?
 
 useradd expense
 
@@ -12,13 +28,24 @@ cd /app
 unzip /tmp/backend.zip
 
 cd /app
-npm install
-dnf install mysql -y
-mysql -h  mysql-dev.vmoturidevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
+
+echo -e "${color} Download NodeJS Dependencies \e[0m"
+npm install &>>$log_file
+echo$?
+
+echo -e "${color} Install MySQL Client to Load Schema \e[0m"
+dnf install mysql -y &>>$log_file
+echo$?
+
+echo -e "${color} Load Schema \e[0m"
+mysql -h  mysql-dev.vmoturidevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+
+echo -e "${color} Starting Backend Service \e[0m"
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl restart backend &>>$log_file
+echo$?
 
 
 
